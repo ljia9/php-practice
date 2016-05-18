d239f32e09c3a191bc731f4f5ca354f4a54d4ebb
Leigh Jia <ljia@RSG5384LeighJia.rsg.lan>
2016-05-18 16:37:23 -0400

    Conway game of life

---
+++ game-of-life/board.php
@@ -0,0 +1,89 @@
+<?php
+
+function printe($string) {
+    printf($string);
+    printf("\r\n");
+}
+
+function trueFalse($sym) {
+    if ($sym == "1") print("True");
+    else print("False");
+}
+
+#####################
+# Class Constructor #
+#####################
+
+class Board {
+
+    /* 
+    private $height = 5;
+    private $width = 5;
+    private $cells = array();
+    */
+
+    public function initialize($arr, $h, $w) {
+        $this->height = $h;
+        $this->width = $w;
+        $this->cells = $arr;
+    }
+
+    public function getHeight() {
+        return $this->height; 
+    }
+    public function getWidth() {
+        return $this->width; 
+    }
+    public function liveCells() {
+        return $this->cells;
+    }
+    
+    private function isAlive($arr) {
+        if (in_array($arr, $this->cells) == true) return true;
+        else return false;
+    }
+    private function neighborTo($arr) {
+        $count = 0;
+        foreach ($this->cells as $val) {
+            $dx = abs($val[0] - $arr[0]);
+            $dy = abs($val[1] - $arr[1]);
+            if ($dx <= 1 and $dy <= 1 and !($dx==0 and $dy==0)) $count++;
+        }
+        return $count;
+    }
+    private function shouldSurvive($arr) {
+        $n = $this->neighborTo($arr);
+        return (($n >= 2 and $n < 4) and $this->isAlive($arr));
+    }
+    private function shouldReproduce($arr) {
+        return (!$this->isAlive($arr) and $this->neighborTo($arr) == 3);
+    }
+
+    public function evolve() {
+        $survivors = array();
+        foreach (range(0, $this->width) as $i) {
+            foreach (range(0, $this->height) as $j) {
+                $arr = array($i, $j);
+                if ($this->shouldSurvive($arr) or $this->shouldReproduce($arr)) array_push($survivors, $arr);
+            }
+        }
+        $b = new Board();
+        $b->initialize($survivors, $this->height, $this->width);
+        return $b;
+    }
+    public function printBoard() {
+        printe("New Generation:");
+        printe("===============");
+        foreach (range(0, $this->width) as $i) {
+            foreach (range(0, $this->height) as $j) {
+                $arr = array($i, $j);
+                if ($this->isAlive($arr) == true) print('#');
+                else print('-');
+            }
+            printe('');
+        }
+        printe('');
+
+    }
+}
+
---
+++ game-of-life/run-board.php
@@ -0,0 +1,23 @@
+<?php
+
+require('game-of-life.php');
+
+#################
+# Test here lol #
+#################
+
+function view($board, $num) {
+    foreach(range(1, $num) as $i) {
+        $board->printBoard();
+        $board = $board->evolve();
+    }
+}
+
+$start = array(array(3,1), array(3,2), array(3,3), array(2,3), array(2,2), array(2,4));
+$blinker = array(array(2,1), array(2,2), array(2,3));
+$test = new Board();
+$test->initialize($start, 7, 7);
+
+view($test, 5);
+
+?>
